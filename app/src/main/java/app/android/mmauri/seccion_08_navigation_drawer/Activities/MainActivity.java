39d415f5a97ff205291c171012d168f6249c280b
Marc from GitKraken <marcmauri92@gmail.com>
2017-11-13 13:30:43 +0100

    Comportamiento del menu

--- app/src/main/java/app/android/mmauri/seccion_08_navigation_drawer/Activities/MainActivity.java
+++ app/src/main/java/app/android/mmauri/seccion_08_navigation_drawer/Activities/MainActivity.java
@@ -1,17 +1,25 @@
 package app.android.mmauri.seccion_08_navigation_drawer.Activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import app.android.mmauri.seccion_08_navigation_drawer.Fragments.AlertsFragment;
+import app.android.mmauri.seccion_08_navigation_drawer.Fragments.EmailFragment;
+import app.android.mmauri.seccion_08_navigation_drawer.Fragments.InfoFragment;
 import app.android.mmauri.seccion_08_navigation_drawer.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,55 @@ public class MainActivity extends AppCompatActivity {
         setToolbar();
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                boolean fragmentTx = false;
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.menu_mail:
+                        fragment = new EmailFragment();
+                        fragmentTx = true;
+                        break;
+                    case R.id.menu_alert:
+                        fragment = new AlertsFragment();
+                        fragmentTx = true;
+                        break;
+                    case R.id.menu_info:
+                        fragment = new InfoFragment();
+                        fragmentTx = true;
+                        break;
+                    case R.id.menu_option_1:
+                        Toast.makeText(getApplicationContext(), "Has clickeado en la opcion 1",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_option_2:
+                        Toast.makeText(getApplicationContext(), "Has clickeado en la opcion 2",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                if (fragmentTx == true) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.content_frame, fragment)
+                            .commit();
+
+                    // Marca el item como clickeado
+                    item.setChecked(true);
+                    // Actualizamos el titulo del toolbar
+                    getSupportActionBar().setTitle(item.getTitle());
+                    // Cerramos el menu
+                    drawerLayout.closeDrawers();
+                }
+
+                return true;
+            }
+        });
     }
 
     private void setToolbar() {
